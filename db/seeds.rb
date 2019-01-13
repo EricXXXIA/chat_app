d863b19e5a928eb955c89623e488817eb59a8b48
Eric XIA <rxia0914@outlook.com>
2019-01-13 16:26:24 -0500

    project complete

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create(username: 'Xia',password: 'password')
+User.create(username: 'Ruo',password: 'password')
+User.create(username: 'Fel',password: 'password')
+User.create(username: 'Lil',password: 'password')
+User.create(username: 'ONN',password: 'password')
